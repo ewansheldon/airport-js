4e98629b13303aad21e17f366ce0b4b4a8310fe6
johnnydee8 <j.dworzynski@me.com>
2016-09-13 15:22:46 +0100

    taking off done

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,14 +1,11 @@
 // 'use strict';
 
-describe("airport", function() {
+describe("airport during sunny weather", function() {
     var airport;
     var plane;
 
     beforeEach(function() {
-      weather = {
-        isStormy: function() {
-
-        }};
+      weather = {isStormy: function(){}};
       airport = new Airport(weather);
       plane = jasmine.createSpyObj('plane',['status']);
       spyOn(weather, 'isStormy').and.returnValue(false);
@@ -23,6 +20,12 @@ describe("airport", function() {
       expect(airport.planes()).toContain(plane);
     });
 
+    it("allows planes to takeoff", function(){
+      airport.land(plane);
+      airport.takeoff(plane)
+      expect(airport.planes()).not.toContain(plane);
+    })
+
 });
 
 describe("airport when stormy", function() {
@@ -30,14 +33,9 @@ describe("airport when stormy", function() {
     var plane;
 
     beforeEach(function() {
-      weather = {
-        isStormy: function() {
-
-        }
-      };
+      weather = {isStormy: function(){}};
       airport = new Airport(weather);
       plane = jasmine.createSpyObj('plane',['status']);
-
       spyOn(weather, 'isStormy').and.returnValue(true);
     });
 
@@ -46,4 +44,10 @@ describe("airport when stormy", function() {
       expect(airport.planes()).not.toContain(plane);
     });
 
+    it("doesnt planes to take off", function() {
+      airport.containedPlanes.push(plane)
+      airport.takeoff(plane)
+      expect(airport.planes()).toContain(plane)
+
+    })
   });
--- src/Airport.js
+++ src/Airport.js
@@ -1,18 +1,20 @@
-// 'use strict';
-// var weatherClass = new Weather();
-function Airport(weatherClass){
-  this.containedPlanes = [];
-  this.weather = weatherClass || new Weather();
+function Airport (weatherClass) {
+  this.containedPlanes = []
+  this.weather = weatherClass || new Weather()
 }
 
-Airport.prototype.planes = function(){
-  return this.containedPlanes;
-};
+Airport.prototype.planes = function () {
+  return this.containedPlanes
+}
 
 Airport.prototype.land = function(aPlane) {
   if (!this.weather.isStormy()) {
-    this.containedPlanes.push(aPlane);
-  } else {
-    console.log('string')
+    this.containedPlanes.push(aPlane)
   }
-};
+Airport.prototype.takeoff = function (aPlane) {
+  if (!this.weather.isStormy()) {
+    var index = this.containedPlanes.indexOf(aPlane)
+    this.containedPlanes.splice(index, 1)
+  }
+}
+}
